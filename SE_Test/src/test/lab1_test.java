e84ff61cd1fd6e6627cdd003d9ca974baede1bb3
omkarpg0931 <omkar0931@gmail.com>
2015-06-24 15:11:31 +0530

    My first Se project

---
+++ SE_Test/src/test/lab1_test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class lab1_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to world of Software Engineering");
+	}
+
+}

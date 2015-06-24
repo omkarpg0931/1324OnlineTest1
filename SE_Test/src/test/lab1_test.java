8bdf011654f664cef4ce0642549b61b83b0844b9
omkarpg0931 <omkar0931@gmail.com>
2015-06-24 15:24:06 +0530

    My First SE lab assignment
    Developer: Omkar Prabhu G	aonkar

--- SE_Test/src/test/lab1_test.java
+++ SE_Test/src/test/lab1_test.java
@@ -5,5 +5,7 @@ public class lab1_test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Welcome to world of Software Engineering");
+			System.out.println("My name is Omkar Prabhu Gaonkar");
+			
 	}
 }
